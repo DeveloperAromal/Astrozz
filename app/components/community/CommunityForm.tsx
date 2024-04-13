cd0f74d89b56e30030cf78f562d0bdc4e50565e5
DeveloperAromal <153208245+DeveloperAromal@users.noreply.github.com>
2024-04-13 14:14:08 +0530

    commited

--- app/components/community/CommunityForm.tsx
+++ app/components/community/CommunityForm.tsx
@@ -36,78 +36,6 @@ const ComplaintForm: FC = () => {
           <div>
             <form onSubmit={handleSubmit(onSubmit)}>
               <div>
-                {/* <div className="flex items-center justify-center">
-                  <div>
-                    <div className="pb-4">
-                      <input
-                        type="text"
-                        placeholder="Full Name"
-                        className="contact-inp w-80 h-10 rounded-md border border-gray-300 bg-white py-3 pl-2 pr-2 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
-                        {...register("name", { required: true })}
-                      />
-                    </div>
-                    <div className="pb-4">
-                      <input
-                        type="tel"
-                        placeholder="Phone Number"
-                        maxLength={10}
-                        className="contact-inp w-80 h-10 rounded-md border border-gray-300 bg-white py-3 pl-2 pr-2 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
-                        {...register("phonenumber", { required: true })}
-                      />
-                    </div>
-                    <div className="pb-4">
-                      <input
-                        type="email"
-                        placeholder="example@gmail.com"
-                        className="contact-inp w-80 h-10 rounded-md border border-gray-300 bg-white py-3 pl-2 pr-2 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
-                        {...register("email", { required: true })}
-                      />
-                    </div>
-                    <div className="pb-4 flex gap-4">
-                      <select
-                        id="class"
-                        className="w-56 h-10 text-black rounded-md complaint-select pl-2 pr-2"
-                        {...register("classNumber", { required: true })}
-                      >
-                        <option value="Select your class">
-                          Select your class
-                        </option>
-                        <option value="Lkg">Lkg</option>
-                        <option value="Ukg">ukg</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                      </select>
-                      <select
-                        id="class"
-                        className="w-20 h-10 text-black rounded-md complaintdiv-select pl-2 pr-2"
-                        {...register("division", { required: true })}
-                      >
-                        <option value="Div">Div</option>
-                        <option value="A">A</option>
-                        <option value="B">B</option>
-                        <option value="C">C</option>
-                      </select>
-                    </div>
-                    <div className="pb-4">
-                      <textarea
-                        rows={4}
-                        placeholder="Type your message"
-                        className=" contact-text w-80  resize-none rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
-                        {...register("message", { required: true })}
-                      ></textarea>
-                    </div>
-                  </div>
-                </div> */}
                 <div className="pb-4">
                   <input
                     type="text"
@@ -197,6 +125,7 @@ const ComplaintForm: FC = () => {
                     type="text"
                     placeholder="Addmission no"
                     maxLength={7}
+                    minLength={7}
                     className="contact-inp w-32 h-10 rounded-md border border-gray-300 bg-white py-3 pl-2 pr-2 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
                     {...register("addmission", { required: true })}
                   />
@@ -227,14 +156,6 @@ const ComplaintForm: FC = () => {
                   </button>
                 </div>
               </div>
-              {/* <div className="flex items-center justify-center">
-                <button
-                  type="submit"
-                  className="hover:shadow-form rounded-md bg-red-700 py-3 px-8 text-base font-semibold text-white outline-none"
-                >
-                  Register
-                </button>
-              </div> */}
             </form>
           </div>
         </div>
